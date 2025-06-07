2620140fd7dc76a9609ab4e3b240c1785b89c70e
vinigani01 <vinigani.01@gmail.com>
2025-06-07 19:13:31 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="debug-visualizer",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Visualizers for VS Code Debug Visualizer extension",
+    author="vinigani",
+    url="https://github.com/vinigani/vscode-debug-visualizer-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
